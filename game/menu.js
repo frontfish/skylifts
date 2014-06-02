3b9eeafaa5fe4386817ab4d85c2052e7cbda8cbf
Christopher Hinstorff <chris@chinstorff.com>
2014-06-02 22:07:07 +0000

    start creating menu

--- game/menu.js
+++ game/menu.js
@@ -2,13 +2,26 @@ Game.Menu = function (game) { };
 
 Game.Menu.prototype = {
     create: function () {
+	game.physics.startSystem(Phaser.Physics.ARCADE);
 	
+	cursors = game.input.keyboard.createCursorKeys();
+
+	player = game.add.sprite(50, h - 180, 'player');
+	player.anchor.setTo(0.5, 1);
+	game.physics.arcade.enable(player);
+	player.body.gravity.y = 1000;
+	player.inAir = true;
+	player.frame = 1;
+	player.collideWorldBounds = true;
+
     },
 
     update: function () {
 	// upon some input:
-	music = game.add.sound('music');
-	music.play('', 0.4, 0.5, true, false);
-	game.state.start('Play');
+	if (false) {
+	    music = game.add.sound('music');
+	    music.play('', 0.4, 0.5, true, false);
+	    game.state.start('Play');
+	}
     }
 };
