575f5d12246d7b8cd71cd052afce585ef3dff995
Christopher Hinstorff <chris@chinstorff.com>
2014-06-03 01:25:40 +0000

    more mobile stuff

--- game/boot.js
+++ game/boot.js
@@ -10,21 +10,49 @@ Game.Boot = function (game) { };
 
 Game.Boot.prototype = {
     preload: function () {
+	game.load.image('loadingBar', 'assets/img/loading_bar.png');
+	game.load.image('loadingFrame', 'assets/img/loading_frame.png');
+    },
+
+    create: function () {
 	if (!game.device.desktop) {
 	    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 	    this.scale.minWidth = 480;
 	    this.scale.minHeight = 320;
 	    this.scale.maxWidth = 1152;
 	    this.scale.maxHeight = 768;
-	    this.scale.refresh();
-	    this.scale.startFullScreen();
+            this.scale.pageAlignHorizontally = true;
+            this.scale.pageAlignVertically = true;
+            this.scale.forceOrientation(true, false);
+            this.scale.hasResized.add(this.gameResized, this);
+            this.scale.enterIncorrectOrientation.add(this.enterIncorrectOrientation, this);
+            this.scale.leaveIncorrectOrientation.add(this.leaveIncorrectOrientation, this);
+            this.scale.setScreenSize(true);
 	}
 
-	game.load.image('loadingBar', 'assets/img/loading_bar.png');
-	game.load.image('loadingFrame', 'assets/img/loading_frame.png');
+	game.state.start('Load');
+    }, 
+
+    gameResized: function (width, height) {
+
+        //  This could be handy if you need to do any extra processing if the game resizes.
+        //  A resize could happen if for example swapping orientation on a device.
+
     },
 
-    create: function () {
-	game.state.start('Load');
+    enterIncorrectOrientation: function () {
+
+        BasicGame.orientated = false;
+
+        document.getElementById('orientation').style.display = 'block';
+
+    },
+
+    leaveIncorrectOrientation: function () {
+
+        BasicGame.orientated = true;
+
+        document.getElementById('orientation').style.display = 'none';
+
     }
 };
